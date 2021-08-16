4d90e4e9cfbe081c6fb80ced4404d46ab20b1a1b
Niroj Thapa <nirozthapa556@gmail.com>
2021-08-16 16:56:11 +1000

    embeded stackView inside ScrollView

---
+++ AutoLayoutPractice/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AutoLayoutPractice
+//
+//  Created by Niroj Thapa on 8/11/21.
+//  Copyright © 2021 com.hello. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func buttonTapped(_ sender: UIButton) {
+      if sender.title(for: .normal) == "X" {
+        sender.setTitle("A very long title for this button", for: .normal)
+      } else {
+        sender.setTitle("X", for: .normal)
+      }
+    }
+
+}
+
---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'AutoLayoutPractice' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+pod 'SkyFloatingLabelTextField', '~> 3.0'
+pod 'IQKeyboardManager'
+
+
+
+end
